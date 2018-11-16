389a64e0b26464c0abc335bd695cc171d55085c7
Bijon Miles <bijonmiles01@gmail.com>
2018-11-16 11:06:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
+const toyAdd = document.querySelector('.add-toy-form')
+const toyList = document.querySelector('#toy-collection')
 
+const TOY_URL = `http://localhost:3000/toys`
 // YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
@@ -13,6 +16,91 @@ addBtn.addEventListener('click', () => {
   } else {
     toyForm.style.display = 'none'
   }
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    fetch(TOY_URL)
+    .then(function(res){
+      return res.json()
+    })
+    .then(function(json){
+      console.log(json);
+
+      json.forEach(function(toy) {
+        toyList.innerHTML += ` <div class="card" data-id="${toy.id}">
+          <h2>${toy.name}</h2>
+          <img src="${toy.image}" class="toy-avatar" />
+          <p>${toy.likes} Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div> `
+      })
+
+      toyAdd.addEventListener('submit', function(event){
+        // debugger
+        event.preventDefault()
+        let name = event.target[0].value
+        let image = event.target[1].value
+        let val = 0
+        // debugger
+        fetch(TOY_URL, {
+          method: "POST",
+
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            name: name,
+            image: image,
+            likes: val
+          })
+        })
+        .then(function(res){
+          return res.json()
+        })
+
+        .then(function(toy){
+
+          toyList.innerHTML += ` <div class="card" data-id="${toy.id}">
+            <h2>${toy.name}</h2>
+            <img src="${toy.image}" class="toy-avatar" />
+            <p>${toy.likes} Likes </p>
+            <button class="like-btn">Like <3</button>
+          </div> `
+        })
+
+      })
+
+      toyList.addEventListener('click', function(event) {
+        if (event.target.className === 'like-btn') {
+          // debugger
+          let likesId = event.target.parentNode.dataset.id
+          let likes = parseInt(event.target.previousElementSibling.innerHTML[0])
+          likes++
+          // debugger
+          event.target.previousElementSibling.innerHTML = likes + " Likes"
+
+          fetch(TOY_URL + `/${likesId}`, {
+            method: "PATCH",
+
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+              likes: likes
+            })
+          })
+          .then(function(res){
+            return res.json()
+          })
+          .then(function(json) {
+            return json
+          })
+
+        }
+      })
+
+    })
 })
 
 
